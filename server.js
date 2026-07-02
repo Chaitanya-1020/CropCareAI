71fbcdb5825a87c8ab28ed387afd5db16c7d8717
Chaitanya-1020 <chaitanyamule107@gmail.com>
2026-07-02 11:27:50 +0530

    Fix MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cron = require("node-cron");
+
 process.on("uncaughtException", (err) => {
   console.error(err.stack);
   process.exit(1);
@@ -10,23 +11,27 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-const db = process.env.DATABASE.replace(
-  "<db_password>",
-  process.env.DATABASE_PASSWORD
-);
+const db = process.env.DATABASE;
 
-mongoose.connect(db, {}).then((con) => {
-  console.log(`Name of the database is ${con.connection.name}`);
-  console.log("Successfully connected to the database");
-});
-
+mongoose
+  .connect(db)
+  .then((con) => {
+    console.log(`Name of the database is ${con.connection.name}`);
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:");
+    console.error(err);
+    process.exit(1);
+  });
 
 const portnumber = process.env.PORT || 3000;
+
 const server = app.listen(portnumber, () => {
   console.log(`App is running on port ${portnumber}`);
 });
 
-process.on("uncaughtException", (err) => {
+process.on("unhandledRejection", (err) => {
   console.error(err);
-  process.exit(1);
+  server.close(() => process.exit(1));
 });
